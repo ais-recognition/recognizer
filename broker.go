378f53ec497eaabcbc9ad3fed3708e009ffd7510
to0 <yuanancai@gmail.com>
2015-03-04 20:47:18 -0500

    broker init

---
+++ broker.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"github.com/surge/glog"
+	"github.com/surgemq/surgemq/service"
+)
+
+var (
+	// keepAlive        int
+	// connectTimeout   int
+	// ackTimeout       int
+	// timeoutRetries   int
+	// authenticator    string
+	// sessionsProvider string
+	// topicsProvider   string
+	cpuprofile       string
+)
+
+func main() {
+	fmt.Println("hi")
+	svr := &service.Server{
+		KeepAlive:        300,           // seconds
+		ConnectTimeout:   2,             // seconds
+		SessionsProvider: "mem",         // keeps sessions in memory
+		Authenticator:    "mockSuccess", // always succeed
+		TopicsProvider:   "mem",         // keeps topic subscriptions in memory
+	}
+
+	var f *os.File
+	var err error
+
+	if cpuprofile != "" {
+		f, err = os.Create(cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		pprof.StartCPUProfile(f)
+	}
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, os.Interrupt, os.Kill)
+	go func() {
+		sig := <-sigchan
+		glog.Errorf("Existing due to trapped signal; %v", sig)
+
+		if f != nil {
+			glog.Errorf("Stopping profile")
+			pprof.StopCPUProfile()
+			f.Close()
+		}
+
+		svr.Close()
+
+		os.Exit(0)
+	}()
+
+	err = svr.ListenAndServe("tcp://:1883")
+	if err != nil {
+		glog.Errorf("surgemq/main: %v", err)
+	}
+
+}
