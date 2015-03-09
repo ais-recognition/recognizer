1c13c0cc056f75a207464868e584fcfb783f1fbb
to0 <yuanancai@gmail.com>
2015-03-09 00:03:48 -0400

    add voiceid

--- worker.py
+++ worker.py
@@ -1,4 +1,6 @@
 import paho.mqtt.client as mqtt
+from voiceid.sr import Voiceid
+from voiceid.db import GMMVoiceDB
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc):
@@ -14,10 +16,19 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+db = GMMVoiceDB('voiceDB')
+db.add_model('yuannancai_wav', 'Yuannan Cai')
+db.add_model('yuzhongji_wav', 'Yuzhong Ji')
+print db.get_speakers()
+
+v = Voiceid(db, 'cyn.m4a')
+v.extract_speakers()
+label = v.get_clusters()[0]
+cluster = v.get_cluster(label)
+print cluster
 
 client.connect("127.0.0.1", 1883, 60)
 # client.connect("iot.eclipse.org", 1883, 60)
-
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
